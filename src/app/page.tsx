3f58f901fad6e628223aa84a54d6b973ef72b012
BucurEva87 <bucur.eva87@gmail.com>
2024-06-22 16:27:27 +0300

    Test env variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
 export default async function Home() {
-  return <h1>Welcome to Oana&apos;s!</h1>
+  return (
+    <>
+      <h1>Welcome to Oana&apos;s!</h1>
+      <ul>
+        <li>{process.env.GOOGLE_CLIENT_ID}</li>
+        <li>{process.env.GOOGLE_CLIENT_SECRET}</li>
+        <li>{process.env.FACEBOOK_CLIENT_ID}</li>
+        <li>{process.env.FACEBOOK_CLIENT_SECRET}</li>
+      </ul>
+    </>
+  )
 }
