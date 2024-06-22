2dce86f1fb363d155072f4ad25ebecd5decc0b6f
BucurEva87 <bucur.eva87@gmail.com>
2024-06-22 16:37:47 +0300

    Reset after testing and success

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,3 @@
 export default async function Home() {
-  return (
-    <>
-      <h1>Welcome to Oana&apos;s!</h1>
-      <ul>
-        <li>{process.env.GOOGLE_CLIENT_ID}</li>
-        <li>{process.env.GOOGLE_CLIENT_SECRET}</li>
-        <li>{process.env.FACEBOOK_CLIENT_ID}</li>
-        <li>{process.env.FACEBOOK_CLIENT_SECRET}</li>
-      </ul>
-    </>
-  )
+  return <h1>Welcome to Oana&apos;s!</h1>
 }
